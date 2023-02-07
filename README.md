9dfcf43963141f61fdd40435d0f9bef17b4c05d0
Charanaraj N <goudacharanraj760@gmail.com>
2023-02-07 11:02:52 +0530

    managing form state and handling form submission

--- README.md
+++ README.md
@@ -5,5 +5,9 @@ npx create-react-app simple-form
 cd simple-form
 npm start
 
-install Formik with NPM
+clean up the defaults and create a components folder inside the src folder. Create new form file in component folder.
+
+Implement a simple HTML form.
+
+Install Formik with NPM
 npm install formik --save
--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,19 +1,52 @@
 import React from 'react'
+import { useFormik } from 'formik'
 
-export default function YoutubeForm() {
+export default function YoutubeForm() { 
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            channel: ''
+        },
+
+        onSubmit: values => {
+            console.log("form data", values)
+        }
+    })
+
+    // console.log("Formik values -", formik.values)
+
   return (
     <div>
-        <form>
+        <form onSubmit={formik.handleSubmit}>
             <label htmlFor='name'>Name</label>
-            <input id='name' type='text' name='name' />
+            <input
+                id='name'
+                type='text'
+                name='name'
+                onChange={formik.handleChange}
+                value={formik.values.name}
+            />
 
             <label htmlFor='email'>Email</label>
-            <input id='email' type='email' name='email' />
+            <input
+                id='email'
+                type='email'
+                name='email'
+                onChange={formik.handleChange}
+                value={formik.values.email}
+            />
 
             <label htmlFor='channel'>Channel</label>
-            <input id='channel' type='text' name='channel' />
+            <input
+                id='channel'
+                type='text'
+                name='channel'
+                onChange={formik.handleChange}
+                value={formik.values.channel}
+            />
 
-            <button>Submit</button>
+            <button type='submit'>Submit</button>
         </form>
     </div>
   )
